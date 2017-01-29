8688468de151ee683789db85cf283cd9f8c67b0e
Claude.fauconnet <claude.fauconnet@neuf.fr>
2017-01-29 17:33:19 +0100

    correction

--- bin/serverParams.js
+++ bin/serverParams.js
@@ -5,8 +5,7 @@
 
 var serverParams={
     neo4jUrl: 'http://neo4j:souslesens@localhost:7474',
-  //  neo4jUrl: 'http://neo4j:vi0lon@vps254642.ovh.net:7474',
-    mongoUrl : "mongodb://127.0.0.1:27017/"
+     mongoUrl : "mongodb://127.0.0.1:27017/"
 }
 
 module.exports = serverParams;
