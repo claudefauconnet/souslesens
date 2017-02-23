bfaae0d552bbe4d6af89572db6312270a8757361
Claude.fauconnet <claude.fauconnet@neuf.fr>
2017-02-23 09:00:45 +0100

    correction

--- bin/sinequa/entities2Neo.js
+++ bin/sinequa/entities2Neo.js
@@ -5,22 +5,22 @@ var exportMongoToNeoBatch = require('../exportMongoToNeo');
 var fs = require('fs');
 var path = require('path');
 var neoImportObjects = require('../neoImportObjects.js');
-
+var existingNodes=[];
 
 //***************************************Params****************************************************
 var toProcessArray = [
     {
         sinequaFilePath: './neoNormes_surete.csv',
         neoSubGraph: 'SinequaSurete',
-        regexArray: ['regexNormeInt', 'regexNormeExt', 'regexLegalLaw', 'regexLegalReg']
+        regexArray: ['regexNormeInt', 'regexNormeExt', 'regexLegalLaw']//, 'regexLegalReg']
     }
 ];
 
 var regeDefs = {
-    regexNormeInt: {regex: "([GS|CR|LI]{2}[- ]MS[- ][A-Z]{3,4}[- ][0-9]{2,4})", sourceLabel: "internalStandard",targetLabel:"internalStandardX",relType:"refersTo"},
-    regexNormeExt: {regex: "([GS|CR|LI]{2}[- ]MS[- ][A-Z]{3,4}[- ][0-9]{2,4})", sourceLabel: "internalStandard",targetLabel:"internalStandardX",relType:"refersTo"},
-    regexLegalLaw: {regex: "(L. *[0-9]+-?[0-9]*-?[0-9]*)", sourceLabel: "law",targetLabel:"lawX",relType:"refersTo"},
-    regexLegalReg: {regex: "(R. *[0-9]+-?[0-9]*-?[0-9]*)", sourceLabel: "regulation",targetLabel:"regulationX",relType:"refersTo"},
+    regexNormeInt: {regex: "([GS|CR|LI]{2}[- ]MS[- ][A-Z]{3,4}[- ][0-9]{2,4})", sourceLabel: "document",targetLabel:"internalStandard",relType:"refersTo"},
+    regexNormeExt: {regex: "([GS|CR|LI]{2}[- ]MS[- ][A-Z]{3,4}[- ][0-9]{2,4})", sourceLabel: "document",targetLabel:"externalStandard",relType:"refersTo"},
+    regexLegalLaw: {regex: "([L|R]. *[0-9]+-?[0-9]*-?[0-9]*)", sourceLabel: "document",targetLabel:"law",relType:"refersTo"},
+    regexLegalReg: {regex: "(R. *[0-9]+-?[0-9]*-?[0-9]*)", sourceLabel: "document",targetLabel:"regulation",relType:"refersTo"},
 }
 
 //******************************************************************************************
@@ -35,17 +35,19 @@ function parse(text, regex, sourceLabel, targetLabel,relType, subGraph){
     for (var i = 0; i < lines.length; i++) {
         var p=lines[i].indexOf('\t');
         var filename=lines[i].substring(0,p);
+        if(existingNodes.indexOf(filename)>-1)// on importe les noeuds qu'une fois pour l'instant !!!!!
+            continue;
         var line=lines[i].substring(p+1);
         var linkedDocs = [];
-        /*      var array;
+           var array;
      while ((array = regex.exec(line)) !== null) {
                 linkedDocs.push(array[0])
         }
-        if (filename) {
+        if (linkedDocs.length>0) {
             newObjs[filename] = linkedDocs;
-        }*/
+        }
 
-    var array=line.split(";");
+  /*  var array=line.split(";");
     for(var i=0;i<array.length;i++){
         if(!array[i].match(/([0-9]*,[0-9]*)/) && linkedDocs.indexOf(array[i]<0) ){
             linkedDocs.push(array[i])
@@ -54,7 +56,7 @@ function parse(text, regex, sourceLabel, targetLabel,relType, subGraph){
     }
         if (filename) {
             newObjs[filename] = linkedDocs;
-        }
+        }*/
 
 
     }
@@ -68,10 +70,12 @@ function writeNodesToNeo(newObjs, sourceLabel,targetLabel, relType, subGraph) {
     var nodesArray = [];
     var relsArray = [];
     for (var key in newObjs) {
+
         var obj = {
             nom: key,
             subGraph: subGraph,
-            id: key
+            id: key,
+            label:sourceLabel
         }
         nodesArray.push(obj);
         for (var i = 0; i < newObjs[key].length; i++) {
@@ -79,15 +83,16 @@ function writeNodesToNeo(newObjs, sourceLabel,targetLabel, relType, subGraph) {
                 nom: newObjs[key][i],
                 subGraph: subGraph,
                 id: newObjs[key][i],
-                label:sourceLabel
+                label:targetLabel
             }
             nodesArray.push(obj);
 
             var relObj = {
                 source: key,
                 target: newObjs[key][i],
-                label:targetLabel
+
             }
+
             relsArray.push(relObj);
 
 
@@ -95,7 +100,10 @@ function writeNodesToNeo(newObjs, sourceLabel,targetLabel, relType, subGraph) {
 
     }
     neoImportObjects.writeNodesToNeoNodes( nodesArray, function (result) {
-        var nodeMappings = result;
+        var nodeMappings = result;;
+        for(var i=0;i<result.length;i++){
+            existingNodes.push(result[i].mongoId);
+        }
 
         var params = {
             sourceField: "source",
