a1b70e1eaa8527732826e7f0edc303e7806f7925
Claude.fauconnet <claude.fauconnet@neuf.fr>
2017-07-09 09:42:55 +0200

    correction

--- bin/serverParams.js
+++ bin/serverParams.js
@@ -8,7 +8,7 @@ var serverParams={
      mongoUrl : "mongodb://127.0.0.1:27017/",
     mongoFetchSize:500,
     elasticUrl:'localhost:9200',
-    uploadMaxSize:100*1000*1000 //20M
+    uploadMaxSize:100*1000*1000 //100M
 }
 
 module.exports = serverParams;
